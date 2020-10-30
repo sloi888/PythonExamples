31156d390c7cdcff37968368fe2d099da6e5e2ed
Christian Morra <cmorra@perscholas.org>
2020-10-30 11:50:12 -0400

    fixed brute force example

--- Brute force.py
+++ Brute force.py
@@ -1,22 +1,26 @@
 # adapted from https://stackoverflow.com/questions/11747254/python-brute-force-algorithm Martjin Pieters' answer
 import random
+from itertools import chain, product
 global guess
+
 length = 5
 pasw = ''
-characters = 'abcdefghijklmnopqrstuvwxyz'
-for i in range(length):
+characters = 'abcdefghijklmnopqrstuvwxyz' # lowercase only to save time
+
+# create a random password designated length
+for x in range(length):
     pasw += characters[random.randint(0, len(characters) - 1)]
-chars = 'abcdefghijklmnopqrstuvwxyz' #only limeted myself to lowercase for simplllicity.
-base = len(chars)+1
-from itertools import chain, product
+
+
 def bruteforce(charset, maxlength):
     return (''.join(candidate)
         for candidate in chain.from_iterable(product(charset, repeat=i)
         for i in range(1, maxlength + 1)))
 
+
 attempts = 0
 for attempt in bruteforce(characters, length):
-    # match it against your password, or whatever
+    # comment out the print line to make it run much faster
     print(attempt)
     attempts += 1
     if attempt == pasw:
