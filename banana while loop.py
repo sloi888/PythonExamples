94345aead85f151dd64e746021d1d7b7155737b5
Christian Morra <cmorra@perscholas.org>
2020-11-18 11:31:23 -0500

    Banana while loop

---
+++ banana while loop.py
@@ -0,0 +1,13 @@
+bananas = 5
+while bananas > -2:
+    if bananas >= 5:
+        print("large bunch")
+    elif 1 <= bananas <= 4:
+        print("small bunch")
+    elif 0 > bananas:
+        print("You're in banana debt")
+    else:
+        print("no bananas")
+    print("You ate a banana")
+    bananas -= 1
+    print(f"You have {bananas} bananas left")
