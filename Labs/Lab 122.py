6f4001c69acdca7fe4a6c1151b5a146124e5252e
Christian Morra <cmorra@perscholas.org>
2021-03-22 22:40:09 -0400

    Put up some alternative, more readable solutions to some of the upcoming labs

---
+++ Labs/Lab 122.py
@@ -0,0 +1,42 @@
+# python3.6
+# coding: utf-8
+
+# store the human preproinsulin sequence in a variable called preproinsulin:
+preproInsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"
+# store the remaining sequence elements of human insulin in variables:
+lsInsulin = "malwmrllpllallalwgpdpaaa"
+bInsulin = "fvnqhlcgshlvealylvcgergffytpkt"
+aInsulin = "giveqcctsicslyqlenycn"
+cInsulin = "rreaedlqvgqvelgggpgagslqplalegslqkr"
+insulin = bInsulin + aInsulin
+
+## Store AA pKR values and N/C-terms in dictionaries:
+pKR = {'y':10.07,'c': 8.18,'k':10.53,'h':6.00,'r':12.48,'d':3.65,'e':4.25}
+## Count the occurrence of each pKR AA in the sequence, and append N/C-terms:
+seqCount = ({x: float(insulin.count(x)) for x in ['y','c','k','h','r','d','e']})
+## Compute the net charge according to the Henderson-Hesselbach equation:
+pH = 0
+# their way
+while (pH <= 14):
+    netCharge = (
+        +(sum({x: ((seqCount[x]*(10**pKR[x]))/((10**pH)+(10**pKR[x]))) for x in ['k','h','r']}.values()))
+        -(sum({x: ((seqCount[x]*(10**pH))/((10**pH)+(10**pKR[x]))) for x in ['y','c','d','e']}.values()))
+    )
+
+    print ('{0:.2f}'.format(pH), netCharge)
+    pH = pH + 1
+
+# a more readable way
+pH = 0
+while (pH <= 14):
+    posCharge = 0
+    # k h and r have positive charges
+    for acid in ['k', 'h', 'r']:
+        posCharge += (seqCount[acid] * (10 ** pKR[acid])) / ((10 ** pH) + (10 ** pKR[acid]))
+    negCharge = 0
+    # y c d and e have negative charges
+    for acid in ['y', 'c', 'd', 'e']:
+        negCharge += seqCount[acid]*(10**pH) / ((10**pH)+(10**pKR[acid]))
+    netCharge = posCharge - negCharge
+    print(f'{pH} {netCharge}')
+    pH = pH + 1
