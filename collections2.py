fe53602316e075f8ca6c705f9181c413a7471381
Christian Morra <cmorra@perscholas.org>
2021-03-26 09:47:03 -0400

    Added collections2.py

---
+++ collections2.py
@@ -0,0 +1,12 @@
+# arrays are defined with []
+array = ["a", "b", "c"]
+
+# tuples are defined with ()
+tuple = ("d", "e", "f")
+
+# dictionaries are defined with {}
+dictionary = {
+    "name" : "Steven",
+    "class" : "AWS",
+    "snitch" : True
+}
